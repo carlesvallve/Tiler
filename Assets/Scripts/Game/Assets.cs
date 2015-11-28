a2155ce71b840d927a284fa7b8b11f7e096fa806
Carles Vallve <snaptothegrid@gmail.com>
2015-11-29 02:27:05 +0900

    Implemented Assets class which loads all assets and store them in dictionaries

---
+++ Assets/Scripts/Game/Assets.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Assets {
+
+	public Dictionary<string, Sprite> dungeon = new Dictionary<string, Sprite>();
+	public Dictionary<string, Sprite> monster = new Dictionary<string, Sprite>();
+
+
+	public Assets () {
+		// Load dungeon assets
+		Sprite[] dungeonAssets = Resources.LoadAll<Sprite>("Tilesets/Dungeon/Images");
+		foreach (Sprite sprite in dungeonAssets) {
+			dungeon.Add(sprite.name, sprite);
+		}
+
+		foreach(KeyValuePair<string,Sprite> asset in dungeon) {
+			Debug.Log(asset.Key + ": " + asset.Value);
+		}
+
+		// Load monster assets
+		Sprite[] monsterAssets = Resources.LoadAll<Sprite>("Tilesets/Monster/Images");
+		foreach (Sprite sprite in monsterAssets) {
+			monster.Add(sprite.name, sprite);
+		}
+
+		foreach(KeyValuePair<string,Sprite> asset in monster) {
+			Debug.Log(asset.Key + ": " + asset.Value);
+		}
+	}
+}
---
+++ Assets/Scripts/Game/Game.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Game : MonoBehaviour {
+
+	public Assets assets;
+
+
+	void Awake () {
+		assets = new Assets();
+		InitGame();
+	}
+
+
+	private void InitGame () {
+		/* grid
+			- layer 1 <Tile> 
+			- layer 2 <Tile> foor overlay -> carpets, traps, blood, items
+			- layer 3 <Tile> walls, doors, entities
+
+
+			- Tile (type, x, y)
+				-Entity ()
+					- Creature ()
+						- Humanoid ()
+
+
+			- we instantiate a prefab associated to the class
+			- all prefabs have a gameObject + sprite child
+			- we assign the sprite by name key from assets dictionary 
+		*/
+	}
+}
