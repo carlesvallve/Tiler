c04b322fa56046bb6719dbb9a17c5ce09272d48a
Carles Vallve <cpujol@wizcorp.jp>
2015-12-07 14:30:17 +0900

    Defiend vision parameters in creatureStats

--- Assets/Scripts/Game/GridElements/Creature/CreatureStats.cs
+++ Assets/Scripts/Game/GridElements/Creature/CreatureStats.cs
@@ -5,18 +5,11 @@ public class CreatureStats {
 	public int level = 1;
 	public int xp = 0;
 
+	// hp
 	public int hpMax = 10;
 	public int hp = 10;
 
-	public float regeneration = 0;
-	public float regenerationRate = 0.1f; // 1 point each 10 turns
-
-	public int hungerMax = 100;
-	public int hunger = 100;
-
-	public int attack = 1;
-	public int defense = 1;
-
+	// basic stats
 	public int str = 1;
 	public int dex = 1;
 	public int con = 1;
@@ -24,5 +17,24 @@ public class CreatureStats {
 	public int wis = 1;
 	public int cha = 1;
 
+	// combat
+	public int attack = 1;
+	public int defense = 1;
+
+	// regeneration
+	public float regeneration = 0;
+	public float regenerationRate = 0.1f; // 1 point each 10 turns
+
+	// hunger
+	public float hunger = 0;
+	public float hungerRate = 0.1f; // 1 point each 10 turns
+
+	// vision
+	public int visionRadius = 5;
+	public int awareness = 1;
+	public int stealth = 1;
+	public int alert = 0;
+
+	// gold
 	public int gold = 0;
 }
