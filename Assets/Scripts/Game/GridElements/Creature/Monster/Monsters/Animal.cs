ee299c6498a0dd3b5f43d0ff2a32a6ed7f7c6f97
Carles Vallve <snaptothegrid@gmail.com>
2015-12-09 01:48:24 +0900

    Cleanup

---
+++ Assets/Scripts/Game/GridElements/Creature/Monster/Monsters/Animal.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Animal : Monster {
+
+	public override void Init (Grid grid, int x, int y, float scale = 1, Sprite asset = null) {
+		base.Init(grid, x, y, scale, asset);
+	}
+}
