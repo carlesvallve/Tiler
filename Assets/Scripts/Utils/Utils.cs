3b15cb78dae4d8f6e0f219b249a78aa9e21542f4
Carles Vallve <cpujol@wizcorp.jp>
2015-12-09 15:28:53 +0900

    Upgraded to unity 5.3 + implemented awesome CasinoRouletteWheel weighted random method

--- Assets/Scripts/Utils/Utils.cs
+++ Assets/Scripts/Utils/Utils.cs
@@ -33,5 +33,47 @@ public static class Utils {
 
 		return str;
 	}
+
+
+	public static MyObj RandomWeight(List<MyObj> list) {
+		// list of objects different 'weight' prop values
+		/*List<MyObj> list = new List<MyObj>(); //[];
+		for (int i = 0; i < 1000; i += 1) {
+			list.Add(new MyObj(i));
+		}*/
+
+		var weightSum = 0;
+		for (var i = 0; i < 1000; i += 1) {
+			weightSum += list[i].weight;
+		}
+
+		// console.log('weightSum', weightSum)
+
+		int selection = 0;
+		int remainingWeight = Random.Range(0, weightSum); //System.Random() * weightSum; // Math.Random() * weightSum;
+
+		// console.log('remainingWeight', remainingWeight)
+		remainingWeight -= list[selection].weight;
+		while (remainingWeight > 0) {
+			remainingWeight -= list[selection].weight;
+			selection += 1;
+		}
+
+		MyObj obj = list[selection];
+
+		// console.log('selection', selection)
+		return obj;
+	}
 	
+}
+
+
+public class MyObj {
+	public int value;
+	public int weight;
+
+	public MyObj (int i) {
+		value = Random.Range(1, 1000);
+		weight = (int)Mathf.Pow(1000 - i, 10);
+	}
 }
