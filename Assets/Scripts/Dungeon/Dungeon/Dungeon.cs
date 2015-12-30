90c2badd2ab58a81d2abfc4ab1dc9f21f825f2ad
Carles Vallve <snaptothegrid@gmail.com>
2015-12-31 01:47:31 +0900

    Cleanup

--- Assets/Scripts/Dungeon/Dungeon/Dungeon.cs
+++ Assets/Scripts/Dungeon/Dungeon/Dungeon.cs
@@ -59,7 +59,7 @@ public class Dungeon : MonoSingleton <Dungeon> {
 		Random.seed = seed;
 
 		// choose between dungeon or cave
-		int r = 100;//Random.Range(1, 100);
+		int r = Random.Range(1, 100);
 
 		if (r <= 50) {
 			// Generate dungeon data
