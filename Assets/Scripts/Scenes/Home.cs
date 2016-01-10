f71fb04fd0de421ec04980545f5dc69f65d9125a
Carles Vallve <snaptothegrid@gmail.com>
2016-01-10 16:18:35 +0900

    Implemented blinking label on home scene

--- Assets/Scripts/Scenes/Home.cs
+++ Assets/Scripts/Scenes/Home.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Home : MonoBehaviour {
@@ -6,7 +7,6 @@ public class Home : MonoBehaviour {
 	private AudioManager sfx;
 	private Navigator navigator;
 	private bool transitioning;
-	
 
 	void Awake () {
 		// force framerate to 60 fps
@@ -20,6 +20,11 @@ public class Home : MonoBehaviour {
 
 		sfx.Play("Audio/Bgm/Scenes/PeacefulTown", 0, 1f, true);
 		sfx.Fade("Audio/Bgm/Scenes/PeacefulTown", 0.5f, 0.5f);
+
+		// start label blink
+		Transform startLabel = transform.Find("Button/TextStart");
+		CanvasGroup group = startLabel.GetComponent<CanvasGroup>();
+		StartCoroutine(Blink(group, 1f));
 	}
 
 
@@ -30,4 +35,33 @@ public class Home : MonoBehaviour {
 		sfx.Fade("Audio/Bgm/Scenes/PeacefulTown", 0, 1f);
 		navigator.Open(sceneName, true);
 	}
+
+
+	public IEnumerator Blink (CanvasGroup group, float delay = 0) {
+		group.alpha = 0;
+		yield return new WaitForSeconds(delay);
+		
+		yield return StartCoroutine(Fade(group, 1, 0.5f, 0.25f));
+		yield return StartCoroutine(Fade(group, 0, 0.25f, 1f));
+
+		StartCoroutine(Blink(group, 0));
+	}
+
+
+	public IEnumerator Fade (CanvasGroup group, float value, float duration = 0.5f, float delay = 0) {
+		yield return new WaitForSeconds(delay);
+
+		float startValue = group.alpha;
+		
+		float elapsedTime = 0;
+		while (elapsedTime < duration) {
+			float t = elapsedTime / duration;
+			group.alpha = Mathf.Lerp(startValue, value, Mathf.SmoothStep(0f, 1f, t)); 
+			elapsedTime += Time.deltaTime;
+			yield return null;
+		}
+		
+		group.alpha = value;
+	}
+
 }
